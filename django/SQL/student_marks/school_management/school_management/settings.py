6a80c8efb9483663a5026ee8f788fc25fc312766
Lavish <lavishrao107@gmail.com>
2025-01-16 03:18:44 +0400

    new file:   django/SQL/student_marks/school_management/Procfile 	modified:   django/SQL/student_marks/school_management/school_management/settings.py 	modified:   django/SQL/student_marks/school_management/staticfiles/school_app/css/styles.css 	new file:   requirements.txt

--- django/SQL/student_marks/school_management/school_management/settings.py
+++ django/SQL/student_marks/school_management/school_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h3xook-avbwmqa_#fz6092jtwtt_&-xr-)fmi!jp)&2n)lg81c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
